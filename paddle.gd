59843e61ca0c7913cdad4bd92008c991a87f2887
Evan Greenfield <evangreenfield@live.com>
2024-08-30 15:01:01 -0400

    initial commit

---
+++ paddle.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (Input.is_action_pressed("left")):
+		self.position.x -= 5
+	if (Input.is_action_pressed("right")):
+		self.position.x += 5
+	
+	pass
